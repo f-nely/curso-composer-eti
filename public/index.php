65a85b436b26a9f684693ae5e8dcc6be94d6e263
nely de faria <nelly.nf54@gmail.com>
2021-11-02 16:38:09 -0300

    Criando arquivo index.php

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+use App\Controllers\HomeController;
+use \ETI\DB\Query;
+
+$query = new Query();
+var_dump($query->query());
+
+$home = new HomeController();
+var_dump($home->index());
+
