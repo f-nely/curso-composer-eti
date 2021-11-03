eb84cd806d784644aa681b37b416686e7a0fa195
nely de faria <nelly.nf54@gmail.com>
2021-11-03 16:56:30 -0300

    Fazendo comentário e chamando método getTrim

--- public/index.php
+++ public/index.php
@@ -2,12 +2,14 @@
 
 require_once '../vendor/autoload.php';
 
-use App\Controllers\HomeController;
-use \ETI\DB\Query;
+//use App\Controllers\HomeController;
+//use \ETI\DB\Query;
 
-$query = new Query();
-var_dump($query->query());
+//$query = new Query();
+//var_dump($query->query());
 
-$home = new HomeController();
-var_dump($home->index());
+//$home = new HomeController();
+//var_dump($home->index());
+
+var_dump(getTrim("  f-nely "));
 
