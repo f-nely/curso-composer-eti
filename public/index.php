07e173d5c79df5007b44aa6824582874c14930aa
nely de faria <nelly.nf54@gmail.com>
2021-11-05 15:45:46 -0300

    Instanciando classe Dompdf

--- public/index.php
+++ public/index.php
@@ -13,5 +13,21 @@ require_once '../vendor/autoload.php';
 
 //var_dump(getTrim("  f-nely "));
 
-$object = new MyClass;
-var_dump($object->myMethod());
+//$object = new MyClass;
+//var_dump($object->myMethod());
+
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+$dompdf->loadHtml('hello world');
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream();
